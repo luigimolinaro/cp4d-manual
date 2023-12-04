954a1e449ce0194a231e3686cda66908b52dcd91
Luigi Molinaro <luigi.molinaro@gmail.com>
2023-12-04 18:05:44 +0100

    Update z_custom-LM.bash

--- z_custom-LM.bash
+++ z_custom-LM.bash
@@ -51,65 +51,11 @@ cpd-cli manage setup-instance-topology \
 --license_acceptance=true \
 --block_storage_class=${STG_CLASS_BLOCK} 
 
+# Privileges Db2U
+# https://www.ibm.com/docs/en/cloud-paks/cp-data/4.8.x?topic=data-specifying-privileges-that-db2u-runs
+
+#WKC Customization : 
+# https://www.ibm.com/docs/en/cloud-paks/cp-data/4.8.x?topic=data-specifying-installation-options-services#install-platform-param-file__wkc-parms__title__1
 
 
-# APPLY-CRIO
-${CLI} manage apply-crio \
-  --openshift_type=${OPENSHIFT_TYPE} \
-  --force=true
-# OPTIONAL (Updating the global image pull secret)
-${CLI} manage add-icr-cred-to-global-pull-secret \
-${IBM_ENTITLEMENT_KEY}
-#APPLY-OLM
-${CLI} manage apply-olm \
---release=${VERSION} \
---components=${COMPONENTS} \
---cpd_operator_ns=${PROJECT_CPD_OPS} \
---cs_ns=${PROJECT_CPFS_OPS}
-#VERIFY operators
-${CLI} manage get-olm-artifacts \
---subscription_ns=${PROJECT_CPFS_OPS}
-#SETUP NS
-${CLI} manage setup-instance-ns \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---cpd_operator_ns=${PROJECT_CPD_OPS} \
---cs_ns=${PROJECT_CPFS_OPS}
-#WKC
-# Create the SCC for Watson Knowledge Catalog
-${CLI} manage apply-crio \
-  --openshift_type=${OPENSHIFT_TYPE} --components=wkc \
-  --force
-#Custom WKC apply-scc
-${CLI}  manage apply-scc \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---components=wkc
-# Install WKC apply-CR (https://www.ibm.com/docs/en/cloud-paks/cp-data/4.5.x?topic=catalog-installing)
-${CLI} manage apply-cr \
---components=wkc \
---release=${VERSION} \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---block_storage_class=${STG_CLASS_BLOCK} \
---file_storage_class=${STG_CLASS_FILE} \
---param-file=/tmp/install-options.yml \
---license_acceptance=true
-#APPLY CR
-${CLI} manage apply-cr \
---components=${COMPONENTS} \
---release=${VERSION} \
---cs_ns=${PROJECT_CPFS_OPS} \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---block_storage_class=${STG_CLASS_BLOCK} \
---file_storage_class=${STG_CLASS_FILE} \
---license_acceptance=true \
---cs_ns=${PROJECT_CPFS_OPS}
-#License Server
-${CLI} manage apply-entitlement \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---entitlement=cpd-enterprise
-#VERIFY Components
-${CLI} manage get-cr-status \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE}
-#FINALIZE
-${CLI} manage get-cpd-instance-details \
---cpd_instance_ns=${PROJECT_CPD_INSTANCE} \
---get_admin_initial_credentials=true
+
